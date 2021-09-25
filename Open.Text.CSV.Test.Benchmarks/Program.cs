c96143765d1ebcc49453b139ccfea5ea49eb4a87
electricessence <5899455+electricessence@users.noreply.github.com>
2021-09-24 17:56:51 -0700

    Added parallel tests.

--- Open.Text.CSV.Test.Benchmarks/Program.cs
+++ Open.Text.CSV.Test.Benchmarks/Program.cs
@@ -6,8 +6,9 @@ namespace Open.Text.CSV.Test.Benchmarks
 	{
 		static void Main()
 		{
-			BenchmarkRunner.Run<CsvFileReadTests>();
+			//BenchmarkRunner.Run<CsvFileReadTests>();
 			//BenchmarkRunner.Run<FileReadMethodTests>();
+			BenchmarkRunner.Run<CsvFileParallelReadTests>();
 		}
 	}
 }
---
+++ Tests/CsvFileParallelReadTests.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open.Text.CSV.Test
+{
+	public class CsvFileParallelReadTests
+	{
+		readonly CsvFileReadTests _root = new();
+
+		[Benchmark]
+		public void GetAllRowsFromFileInParallel()
+		{
+			Parallel.Invoke(
+				_root.GetAllRowsFromFile,
+				_root.GetAllRowsFromFile,
+				_root.GetAllRowsFromFile,
+				_root.GetAllRowsFromFile
+			);
+		}
+
+		[Benchmark]
+		public Task GetAllRowsFromFileAsyncParallel()
+		{
+			return Task.WhenAll(
+				_root.GetAllRowsFromFileAsync(),
+				_root.GetAllRowsFromFileAsync(),
+				_root.GetAllRowsFromFileAsync(),
+				_root.GetAllRowsFromFileAsync()
+			);
+		}
+	}
+}
